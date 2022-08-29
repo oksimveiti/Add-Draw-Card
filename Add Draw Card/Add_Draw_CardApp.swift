f54dcf60f2a56ae95dfbee4d60fc5d306616af6a
Semih Cetin <semihcettin@gmail.com>
2022-08-29 18:02:00 +0300

    Initial Commit

---
+++ Add Draw Card/Add_Draw_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Add_Draw_CardApp.swift
+//  Add Draw Card
+//
+//  Created by Semih Cetin on 29.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Add_Draw_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Add Draw Card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Add Draw Card
+//
+//  Created by Semih Cetin on 29.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
